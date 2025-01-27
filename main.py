01103b50986344d54bfffd27d326930ff4c5de97
Kamesh Singh <radhakrishnapg152627@gmail.com>
2025-01-27 21:54:24 +0530

    selecting word

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 #Radhe Radhe 
 import random
-from collections import Counter
+from collections import Counter  #To calculate the number of occurrances in the provided sequence for all the leements
+
+words=['apple', 'banana', 'mango', 'strawberry', 
+'orange', 'grape', 'pineapple', 'apricot', 'lemon', 'coconut', 'watermelon' 
+'cherry', 'papaya', 'berry', 'peach', 'lychee', 'muskmelon']
+
+word=random.choice(words)
+
+
