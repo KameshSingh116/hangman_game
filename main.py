8a06fa7f59cab5f4903c091aadd4c887f61656c3
Kamesh Singh <radhakrishnapg152627@gmail.com>
2025-01-27 22:18:42 +0530

    input validation

--- main.py
+++ main.py
@@ -13,3 +13,39 @@ if __name__=='__main__':
     print("Start Guessing")
     print("*...Hint...*")
     print(f"Its a fruit with {size} letters")
+
+    for i in word:
+        print('_', end=' ')
+    print()
+
+    playing=True
+    letter_guessed=''
+    chances=size+2
+    correct=0  #used to tell the number of correctly guessed letters in the selected word.
+    flag=0     #used to determine whether the word is complete;ly guessed or not.
+    
+
+    while(chances>0) and flag==0:
+
+        try:
+            guess=str(input("Enter the letter to guess:"))
+
+        except:
+            print("Enter only letters!")
+            continue
+
+        if not guess.isalpha():
+            print("Enter only letters!")
+
+        elif len(guess)>1:
+            print("Enter only single letter!")
+            continue
+
+        elif guess in letter_guessed:
+            print("You have already guessed the letter!")
+            continue
+
+        
+
+        
+
