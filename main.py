9f13234b05092c67f6927b7eeb6096dc16185798
Kamesh Singh <radhakrishnapg152627@gmail.com>
2025-01-27 22:00:57 +0530

    hints

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ import random
 from collections import Counter  #To calculate the number of occurrances in the provided sequence for all the leements
 
 words=['apple', 'banana', 'mango', 'strawberry', 
-'orange', 'grape', 'pineapple', 'apricot', 'lemon', 'coconut', 'watermelon' 
+'orange', 'grape', 'pineapple', 'apricot', 'lemon', 'coconut', 'watermelon', 
 'cherry', 'papaya', 'berry', 'peach', 'lychee', 'muskmelon']
 
 word=random.choice(words)
+size=len(word)
 
-
+if __name__=='__main__':
+    print("Start Guessing")
+    print("*...Hint...*")
+    print(f"Its a fruit with {size} letters")
